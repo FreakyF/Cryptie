78274eb948fd7876579ff0c28dd3df83ad13ab7d
Kamil Fudala <kamil.fudala44@gmail.com>
2025-05-06 20:05:01 +0200

    Fix validation rules.

--- src/Cryptie.Common/Features/Authentication/Validators/RegisterRequestValidator.cs
+++ src/Cryptie.Common/Features/Authentication/Validators/RegisterRequestValidator.cs
@@ -9,7 +9,7 @@ public class RegisterRequestValidator : AbstractValidator<RegisterRequestDto>
     {
         RuleFor(x => x.Login)
             .Cascade(CascadeMode.Stop)
-            .NotEmpty().WithMessage("Username cannot be empty")
+            .Must(v => !string.IsNullOrWhiteSpace(v)).WithMessage("Username cannot be empty")
             .Matches("^[A-Za-z0-9]+$").WithMessage("Username contains invalid characters")
             .MinimumLength(5).WithMessage("Username must be at least 5 characters long")
             .MaximumLength(20).WithMessage("Username cannot exceed 20 characters");
@@ -17,12 +17,11 @@ public class RegisterRequestValidator : AbstractValidator<RegisterRequestDto>
         RuleFor(x => x.DisplayName)
             .Cascade(CascadeMode.Stop)
             .NotEmpty().WithMessage("Display name cannot be empty")
-            .MinimumLength(1).WithMessage("Display name must be at least 1 characters long")
             .MaximumLength(20).WithMessage("Display name cannot exceed 20 characters");
 
         RuleFor(x => x.Password)
             .Cascade(CascadeMode.Stop)
-            .NotEmpty().WithMessage("Password cannot be empty")
+            .Must(v => !string.IsNullOrWhiteSpace(v)).WithMessage("Password cannot be empty")
             .Matches(@"^\P{C}+$").WithMessage("Password contains invalid characters")
             .MinimumLength(8).WithMessage("Password must be at least 8 characters long")
             .MaximumLength(20).WithMessage("Password cannot exceed 20 characters")
