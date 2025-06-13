94f3b738c765a3166a651e5794b1625cc5c4c7ad
Jan Chojnacki <jan@chojnacki.dev>
2025-06-13 16:07:15 +0200

    .

---
+++ src/Cryptie.Server/Features/GroupManagement/GroupManagementController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cryptie.Server.Features.GroupManagment;
+
+[ApiController]
+[Route("group")]
+public class GroupManagementController: ControllerBase
+{
+    
+} 
