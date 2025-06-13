7f5d77278fbf4818cc856e2375e58ea0bf0ade69
Kamil Fudala <kamil.fudala44@gmail.com>
2025-06-13 19:31:24 +0200

    Add BottomToColumnSpanConverter

---
+++ src/Cryptie.Client/Features/Messages/Converters/BottomToColumnSpanConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Cryptie.Client.Features.Messages.Converters;
+
+public class BottomToColumnSpanConverter : IValueConverter
+{
+    public object Convert(object? value, Type t, object? p, CultureInfo c)
+        => value as bool? == true ? 1 : 2;
+
+    public object ConvertBack(object? v, Type t, object? p, CultureInfo c)
+        => throw new NotSupportedException();
+}
