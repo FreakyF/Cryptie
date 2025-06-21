7005022ea4e49f3489a787bcf49315e00f2d1bda
Kamil Fudala <kamil.fudala44@gmail.com>
2025-06-21 15:42:10 +0200

    Show current username in AccountView input.

--- src/Cryptie.Client/Features/Account/ViewModels/AccountViewModel.cs
+++ src/Cryptie.Client/Features/Account/ViewModels/AccountViewModel.cs
@@ -2,6 +2,7 @@
 using Cryptie.Client.Core.Base;
 using Cryptie.Client.Core.Navigation;
 using Cryptie.Client.Features.Authentication.Services;
+using Cryptie.Client.Features.Menu.State;
 using ReactiveUI;
 
 namespace Cryptie.Client.Features.Account.ViewModels;
@@ -10,19 +11,37 @@ public class AccountViewModel : RoutableViewModelBase
 {
     private readonly IKeychainManagerService _keychain;
     private readonly IShellCoordinator _shell;
+    private readonly IUserState _userState;
 
-    public AccountViewModel(IScreen hostScreen,
+    private string? _username;
+
+    public AccountViewModel(
+        IScreen hostScreen,
         IKeychainManagerService keychain,
-        IShellCoordinator shell)
+        IShellCoordinator shell,
+        IUserState userState)
         : base(hostScreen)
     {
         _keychain = keychain;
         _shell = shell;
+        _userState = userState;
+
+        _username = _userState.Username;
 
         SignOutCommand = ReactiveCommand.Create(ExecuteLogout);
     }
 
     public ReactiveCommand<Unit, Unit> SignOutCommand { get; }
+
+    public string? Username
+    {
+        get => _username;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _username, value);
+            _userState.Username = value;
+        }
+    }
 
     private void ExecuteLogout()
     {
