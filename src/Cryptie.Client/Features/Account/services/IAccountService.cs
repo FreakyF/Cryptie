82e8ac3027145eb4999f89da7a7315b28871edb2
Kamil Fudala <kamil.fudala44@gmail.com>
2025-06-27 16:26:07 +0200

    Add changing display name account logic.

---
+++ src/Cryptie.Client/Features/Account/services/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Cryptie.Common.Features.UserManagement.DTOs;
+
+namespace Cryptie.Client.Features.Account.services;
+
+public interface IAccountService
+{
+    Task ChangeUserDisplayNameAsync(
+        UserDisplayNameRequestDto userDisplayNameRequest,
+        CancellationToken cancellationToken = default);
+}
---
+++ src/Cryptie.Common/Features/Account/Validators/UserDisplayNameRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using Cryptie.Common.Features.UserManagement.DTOs;
+using FluentValidation;
+
+namespace Cryptie.Common.Features.Account.Validators;
+
+public class UserDisplayNameRequestValidator : AbstractValidator<UserDisplayNameRequestDto>
+{
+    public UserDisplayNameRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .Cascade(CascadeMode.Stop)
+            .Must(v => !string.IsNullOrWhiteSpace(v)).WithMessage("Username cannot be empty")
+            .Matches("^[A-Za-z0-9]+$").WithMessage("Username contains invalid characters")
+            .MinimumLength(5).WithMessage("Username must be at least 5 characters long")
+            .MaximumLength(20).WithMessage("Username cannot exceed 20 characters");
+    }
+}
