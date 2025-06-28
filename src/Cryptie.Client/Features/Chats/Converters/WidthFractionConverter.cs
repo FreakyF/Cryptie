e80413d3875c04906832557eea8781aadf982d06
Kamil Fudala <kamil.fudala44@gmail.com>
2025-06-28 18:19:13 +0200

    Add mock Chat view.

---
+++ src/Cryptie.Client/Features/Chats/Converters/WidthFractionConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Cryptie.Client.Features.Chats.Converters;
+
+public class WidthFractionConverter : IValueConverter
+{
+    public double Fraction { get; set; } = 0.6;
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not double w)
+        {
+            return 0d;
+        }
+
+        var f = Fraction;
+
+        if (parameter is string s && double.TryParse(s, out var p))
+            f = p;
+
+        return w * f;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotSupportedException();
+}
