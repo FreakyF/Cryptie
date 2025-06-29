ffcec61208c9e0917d9b3d71b6000db2d3229c45
Jakub Babiarski <babiarski.jakub@gmail.com>
2025-06-29 17:55:30 +0200

    .

---
+++ src/Cryptie.Client.Tests/Features/Authentication/Models/LoginModelTest.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using Cryptie.Client.Features.Authentication.Models;
+using Xunit;
+
+namespace Cryptie.Client.Tests.Features.Authentication.Models;
+
+public class LoginModelTests
+{
+    [Fact]
+    public void LoginModel_Property_Setters_Work_Correctly()
+    {
+        // Arrange
+        var model = new LoginModel();
+        var changedProperties = new List<string>();
+        model.PropertyChanged += (_, e) => changedProperties.Add(e.PropertyName!);
+
+        // Act
+        model.Username = "testuser";
+        model.Password = "securepass";
+
+        // Assert
+        Assert.Equal("testuser", model.Username);
+        Assert.Equal("securepass", model.Password);
+        Assert.Contains("Username", changedProperties);
+        Assert.Contains("Password", changedProperties);
+    }
+
+    [Fact]
+    public void LoginModel_DoesNotRaiseEvent_WhenValueIsUnchanged()
+    {
+        // Arrange
+        var model = new LoginModel();
+        model.Username = "initial";
+        model.Password = "123";
+
+        var raised = false;
+        model.PropertyChanged += (_, e) => raised = true;
+
+        // Act
+        model.Username = "initial";  // No change
+        model.Password = "123";      // No change
+
+        // Assert
+        Assert.False(raised);
+    }
+}
---
+++ src/Cryptie.Client.Tests/Features/Authentication/Models/TotpQrSetupModelTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Cryptie.Client.Features.Authentication.Models;
+using Xunit;
+
+namespace Cryptie.Client.Tests.Features.Authentication.Models;
+
+public class TotpQrSetupModelTests
+{
+    [Fact]
+    public void TotpQrSetupModel_Properties_SetCorrectly_And_RaisePropertyChanged()
+    {
+        // Arrange
+        var model = new TotpQrSetupModel();
+        var changedProperties = new List<string>();
+        model.PropertyChanged += (_, e) => changedProperties.Add(e.PropertyName!);
+
+        var expectedSecret = "MYSECRET123";
+        var expectedToken = Guid.NewGuid();
+
+        // Act
+        model.Secret = expectedSecret;
+        model.TotpToken = expectedToken;
+
+        // Assert
+        Assert.Equal(expectedSecret, model.Secret);
+        Assert.Equal(expectedToken, model.TotpToken);
+
+        Assert.Contains(nameof(model.Secret), changedProperties);
+        Assert.Contains(nameof(model.TotpToken), changedProperties);
+    }
+
+    [Fact]
+    public void TotpQrSetupModel_DoesNotRaisePropertyChanged_WhenValueUnchanged()
+    {
+        // Arrange
+        var token = Guid.NewGuid();
+        var model = new TotpQrSetupModel
+        {
+            Secret = "unchanged",
+            TotpToken = token
+        };
+
+        var raised = false;
+        model.PropertyChanged += (_, _) => raised = true;
+
+        // Act
+        model.Secret = "unchanged";
+        model.TotpToken = token;
+
+        // Assert
+        Assert.False(raised);
+    }
+}
