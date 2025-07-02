cb0d690d21a3c29f957838fcd4b8608802f18b70
Jakub Babiarski <babiarski.jakub@gmail.com>
2025-07-02 16:52:05 +0200

    .

---
+++ src/Cryptie.Server.Tests/Features/KeysManagement/Controllers/KeysManagementControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using Cryptie.Common.Features.KeysManagement.DTOs;
+using Cryptie.Server.Features.KeysManagement.Controllers;
+using Cryptie.Server.Features.KeysManagement.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Cryptie.Server.Tests.Features.KeysManagement.Controllers;
+
+public class KeysManagementControllerTests
+{
+    private readonly Mock<IKeysManagementService> _serviceMock;
+    private readonly KeysManagementController _controller;
+
+    public KeysManagementControllerTests()
+    {
+        _serviceMock = new Mock<IKeysManagementService>();
+        _controller = new KeysManagementController(_serviceMock.Object);
+    }
+
+    [Fact]
+    public void getUserKey_ReturnsServiceResult()
+    {
+        // Arrange
+        var request = new GetUserKeyRequestDto();
+        var expectedResult = new OkResult();
+        _serviceMock.Setup(s => s.getUserKey(request)).Returns(expectedResult);
+
+        // Act
+        var result = _controller.getUserKey(request);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+        _serviceMock.Verify(s => s.getUserKey(request), Times.Once);
+    }
+
+    [Fact]
+    public void saveUserKeys_ReturnsServiceResult()
+    {
+        // Arrange
+        var request = new SaveUserKeysRequestDto();
+        var expectedResult = new OkResult();
+        _serviceMock.Setup(s => s.saveUserKeys(request)).Returns(expectedResult);
+
+        // Act
+        var result = _controller.saveUserKeys(request);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+        _serviceMock.Verify(s => s.saveUserKeys(request), Times.Once);
+    }
+}
+
