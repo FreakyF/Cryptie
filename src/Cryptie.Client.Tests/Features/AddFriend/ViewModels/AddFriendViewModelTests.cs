3e3e9eb7cd3b25d780d445bac279108fd40a1778
Jakub Babiarski <babiarski.jakub@gmail.com>
2025-07-02 23:33:04 +0200

    .

--- src/Cryptie.Client.Tests/Features/AddFriend/ViewModels/AddFriendViewModelTests.cs
+++ src/Cryptie.Client.Tests/Features/AddFriend/ViewModels/AddFriendViewModelTests.cs
@@ -1,47 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
 using Cryptie.Client.Core.Services;
+using Cryptie.Client.Encryption;
 using Cryptie.Client.Features.AddFriend.Services;
 using Cryptie.Client.Features.AddFriend.ViewModels;
 using Cryptie.Client.Features.Menu.State;
+using Cryptie.Common.Features.KeysManagement.DTOs;
 using Cryptie.Common.Features.UserManagement.DTOs;
 using FluentValidation;
 using FluentValidation.Results;
 using Moq;
 using ReactiveUI;
-using System.Reactive;
-using System.Reactive.Linq;
-using System.Reactive.Threading.Tasks;
 using Xunit;
 
-namespace Cryptie.Client.Tests.Features.AddFriend.ViewModels
+namespace Cryptie.Client.Tests.Features.AddFriend
 {
     public class AddFriendViewModelTests
     {
-        private readonly Mock<IFriendsService> _friendsServiceMock = new();
-        private readonly Mock<IUserState> _userStateMock = new();
-        private readonly Mock<IValidator<AddFriendRequestDto>> _validatorMock = new();
-        private readonly Mock<IScreen> _screenMock = new();
-        private readonly Mock<IUserDetailsService> _userDetailsServiceMock = new();
-        private readonly Mock<IKeyService> _keyServiceMock = new();
-        private AddFriendViewModel CreateVm() => new(
-            _screenMock.Object,
-            _friendsServiceMock.Object,
-            _userDetailsServiceMock.Object,
-            _keyServiceMock.Object,
-            _validatorMock.Object,
-            _userStateMock.Object);
+        private readonly Mock<IFriendsService> _friendsService = new();
+        private readonly Mock<IKeyService> _keyService = new();
+        private readonly Mock<IUserDetailsService> _userDetailsService = new();
+        private readonly Mock<IValidator<AddFriendRequestDto>> _validator = new();
+        private readonly Mock<IUserState> _userState = new();
+        private readonly Mock<IScreen> _screen = new();
+
+        private AddFriendViewModel CreateVm()
+        {
+            return new AddFriendViewModel(
+                _screen.Object,
+                _friendsService.Object,
+                _userDetailsService.Object,
+                _keyService.Object,
+                _validator.Object,
+                _userState.Object
+            );
+        }
 
         [Fact]
-        public void Constructor_InitializesProperties()
+        public void Constructor_InitializesPropertiesAndCommands()
         {
             var vm = CreateVm();
             Assert.NotNull(vm.SendFriendRequest);
             Assert.Equal(string.Empty, vm.FriendInput);
-            Assert.Equal(string.Empty, vm.ConfirmationMessage);
         }
 
         [Fact]
@@ -56,98 +63,135 @@ namespace Cryptie.Client.Tests.Features.AddFriend.ViewModels
         public void ConfirmationMessage_Property_Works()
         {
             var vm = CreateVm();
-            // Nie można ustawić ConfirmationMessage bezpośrednio, więc testujemy pośrednio przez logikę czyszczenia
-            vm.ErrorMessage = "err";
-            Assert.Equal(string.Empty, vm.ConfirmationMessage);
-            vm.FriendInput = "test"; // nie ustawia ConfirmationMessage, ale testuje setter
+            var prop = vm.GetType().GetProperty("ConfirmationMessage");
+            prop.SetValue(vm, "ok");
+            Assert.Equal("ok", prop.GetValue(vm));
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData("not-a-guid", false)]
+        [InlineData("b8a7e6e2-2b2e-4e2a-8e2e-2b2e4e2a8e2e", true)]
+        public void IsSessionTokenValid_Works(string token, bool expected)
+        {
+            var method = typeof(AddFriendViewModel).GetMethod("IsSessionTokenValid", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+            var args = new object[] { token, null };
+            var result = (bool)method.Invoke(null, args);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("user", "user", true)]
+        [InlineData("user", "USER", true)]
+        [InlineData("user", "other", false)]
+        public void IsAddingSelf_Works(string user, string friend, bool expected)
+        {
+            var method = typeof(AddFriendViewModel).GetMethod("IsAddingSelf", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+            var result = (bool)method.Invoke(null, new object[] { user, friend });
+            Assert.Equal(expected, result);
         }
 
         [Fact]
-        public async Task AddFriendAsync_InvalidToken_SetsError()
+        public void GenerateAesKey_ReturnsKey()
+        {
+            var method = typeof(AddFriendViewModel).GetMethod("GenerateAesKey", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+            var key = (byte[])method.Invoke(null, null);
+            Assert.NotNull(key);
+            Assert.True(key.Length > 0);
+        }
+
+        [Fact]
+        public void SetGenericError_SetsErrorMessage()
         {
             var vm = CreateVm();
-            _userStateMock.SetupGet(x => x.SessionToken).Returns("");
-            await vm.SendFriendRequest.Execute().ToTask();
+            var method = typeof(AddFriendViewModel).GetMethod("SetGenericError", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+            method.Invoke(vm, null);
             Assert.Equal("An error occurred. Please try again.", vm.ErrorMessage);
         }
 
         [Fact]
-        public async Task AddFriendAsync_AddSelf_SetsError()
+        public async Task AddFriendAsync_InvalidSessionToken_SetsGenericError()
         {
             var vm = CreateVm();
-            _userStateMock.SetupGet(x => x.SessionToken).Returns(Guid.NewGuid().ToString());
-            _userStateMock.SetupGet(x => x.Username).Returns("user");
+            _userState.SetupGet(x => x.SessionToken).Returns((string)null);
+            vm.FriendInput = "friend";
+            await vm.SendFriendRequest.Execute().Catch(Observable.Return(Unit.Default));
+            Assert.Equal("An error occurred. Please try again.", vm.ErrorMessage);
+        }
+
+        [Fact]
+        public async Task AddFriendAsync_AddingSelf_SetsError()
+        {
+            var vm = CreateVm();
+            _userState.SetupGet(x => x.SessionToken).Returns(Guid.NewGuid().ToString());
+            _userState.SetupGet(x => x.Username).Returns("user");
             vm.FriendInput = "user";
-            await vm.SendFriendRequest.Execute().ToTask();
+            await vm.SendFriendRequest.Execute().Catch(Observable.Return(Unit.Default));
             Assert.Equal("You cannot add yourself!", vm.ErrorMessage);
         }
 
         [Fact]
-        public async Task AddFriendAsync_ValidationFails_SetsError()
+        public async Task AddFriendAsync_InvalidFriendRequest_SetsError()
         {
             var vm = CreateVm();
-            _userStateMock.SetupGet(x => x.SessionToken).Returns(Guid.NewGuid().ToString());
-            _userStateMock.SetupGet(x => x.Username).Returns("user");
-            vm.FriendInput = "other";
-            _validatorMock.Setup(x => x.ValidateAsync(It.IsAny<AddFriendRequestDto>(), It.IsAny<CancellationToken>()))
-                .ReturnsAsync(new ValidationResult(new[] { new ValidationFailure("Friend", "not found") }));
-            await vm.SendFriendRequest.Execute().ToTask();
+            _userState.SetupGet(x => x.SessionToken).Returns(Guid.NewGuid().ToString());
+            _userState.SetupGet(x => x.Username).Returns("user");
+            vm.FriendInput = "friend";
+            _validator.Setup(x => x.ValidateAsync(It.IsAny<AddFriendRequestDto>(), It.IsAny<CancellationToken>())).ReturnsAsync(new ValidationResult(new List<ValidationFailure> { new("Friend", "err") }));
+            await vm.SendFriendRequest.Execute().Catch(Observable.Return(Unit.Default));
             Assert.Equal("User not found!", vm.ErrorMessage);
         }
 
         [Fact]
-        public async Task AddFriendAsync_Success_SetsConfirmation()
+        public async Task AddFriendAsync_UserNotFound_SetsError()
         {
             var vm = CreateVm();
-            _userStateMock.SetupGet(x => x.SessionToken).Returns(Guid.NewGuid().ToString());
-            _userStateMock.SetupGet(x => x.Username).Returns("user");
-            vm.FriendInput = "other";
-            _validatorMock.Setup(x => x.ValidateAsync(It.IsAny<AddFriendRequestDto>(), It.IsAny<CancellationToken>()))
-                .ReturnsAsync(new ValidationResult());
-            _friendsServiceMock.Setup(x => x.AddFriendAsync(It.IsAny<AddFriendRequestDto>(), It.IsAny<CancellationToken>()))
-                .Returns(Task.CompletedTask);
-            await vm.SendFriendRequest.Execute().ToTask();
-            Assert.Equal(string.Empty, vm.ConfirmationMessage);
-            Assert.Equal("other", vm.FriendInput);
-        }
-
-        [Fact]
-        public async Task AddFriendAsync_HttpNotFound_SetsUserNotFound()
-        {
-            var vm = CreateVm();
-            _userStateMock.SetupGet(x => x.SessionToken).Returns(Guid.NewGuid().ToString());
-            _userStateMock.SetupGet(x => x.Username).Returns("user");
-            vm.FriendInput = "other";
-            _validatorMock.Setup(x => x.ValidateAsync(It.IsAny<AddFriendRequestDto>(), It.IsAny<CancellationToken>()))
-                .ReturnsAsync(new ValidationResult());
-            _friendsServiceMock.Setup(x => x.AddFriendAsync(It.IsAny<AddFriendRequestDto>(), It.IsAny<CancellationToken>()))
-                .ThrowsAsync(new HttpRequestException("not found", null, HttpStatusCode.NotFound));
-            await vm.SendFriendRequest.Execute().ToTask();
+            _userState.SetupGet(x => x.SessionToken).Returns(Guid.NewGuid().ToString());
+            _userState.SetupGet(x => x.Username).Returns("user");
+            vm.FriendInput = "friend";
+            _validator.Setup(x => x.ValidateAsync(It.IsAny<AddFriendRequestDto>(), It.IsAny<CancellationToken>())).ReturnsAsync(new ValidationResult());
+            _userDetailsService.Setup(x => x.GetGuidFromLoginAsync(It.IsAny<GuidFromLoginRequestDto>(), It.IsAny<CancellationToken>())).ThrowsAsync(new HttpRequestException(null, null, HttpStatusCode.NotFound));
+            await vm.SendFriendRequest.Execute().Catch(Observable.Return(Unit.Default));
             Assert.Equal("User not found!", vm.ErrorMessage);
         }
 
         [Fact]
-        public async Task AddFriendAsync_OtherException_SetsGenericError()
+        public async Task AddFriendAsync_CertificateNull_SetsGenericError()
         {
             var vm = CreateVm();
-            _userStateMock.SetupGet(x => x.SessionToken).Returns(Guid.NewGuid().ToString());
-            _userStateMock.SetupGet(x => x.Username).Returns("user");
-            vm.FriendInput = "other";
-            _validatorMock.Setup(x => x.ValidateAsync(It.IsAny<AddFriendRequestDto>(), It.IsAny<CancellationToken>()))
-                .ReturnsAsync(new ValidationResult());
-            _friendsServiceMock.Setup(x => x.AddFriendAsync(It.IsAny<AddFriendRequestDto>(), It.IsAny<CancellationToken>()))
-                .ThrowsAsync(new Exception("fail"));
-            await vm.SendFriendRequest.Execute().ToTask();
+            _userState.SetupGet(x => x.SessionToken).Returns(Guid.NewGuid().ToString());
+            _userState.SetupGet(x => x.Username).Returns("user");
+            vm.FriendInput = "friend";
+            _validator.Setup(x => x.ValidateAsync(It.IsAny<AddFriendRequestDto>(), It.IsAny<CancellationToken>())).ReturnsAsync(new ValidationResult());
+            _userDetailsService.Setup(x => x.GetGuidFromLoginAsync(It.IsAny<GuidFromLoginRequestDto>(), It.IsAny<CancellationToken>())).ReturnsAsync(new GuidFromLoginResponseDto { UserId = Guid.NewGuid() });
+            _keyService.Setup(x => x.GetUserKeyAsync(It.IsAny<GetUserKeyRequestDto>(), It.IsAny<CancellationToken>())).ReturnsAsync((GetUserKeyResponseDto)null);
+            await vm.SendFriendRequest.Execute().Catch(Observable.Return(Unit.Default));
+            Assert.Equal("An error occurred. Please try again.", vm.ErrorMessage);
+        }
+
+        
+
+        [Fact]
+        public async Task TryAddFriend_UserNotFound_SetsError()
+        {
+            var vm = CreateVm();
+            var method = typeof(AddFriendViewModel).GetMethod("TryAddFriend", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+            _friendsService.Setup(x => x.AddFriendAsync(It.IsAny<AddFriendRequestDto>(), It.IsAny<CancellationToken>())).ThrowsAsync(new HttpRequestException(null, null, HttpStatusCode.NotFound));
+            var result = await (Task<bool>)method.Invoke(vm, new object[] { new AddFriendRequestDto(), CancellationToken.None });
+            Assert.False(result);
             Assert.Equal("User not found!", vm.ErrorMessage);
         }
 
         [Fact]
-        public void ErrorMessage_And_ConfirmationMessage_ClearEachOther()
+        public async Task TryAddFriend_Throws_SetsGenericError()
         {
             var vm = CreateVm();
-            vm.ErrorMessage = "err";
-            Assert.Equal(string.Empty, vm.ConfirmationMessage);
-            // Nie można ustawić ConfirmationMessage bezpośrednio, więc testujemy czyszczenie przez ErrorMessage
+            var method = typeof(AddFriendViewModel).GetMethod("TryAddFriend", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+            _friendsService.Setup(x => x.AddFriendAsync(It.IsAny<AddFriendRequestDto>(), It.IsAny<CancellationToken>())).ThrowsAsync(new Exception());
+            var result = await (Task<bool>)method.Invoke(vm, new object[] { new AddFriendRequestDto(), CancellationToken.None });
+            Assert.False(result);
+            Assert.Equal("An error occurred. Please try again.", vm.ErrorMessage);
         }
     }
 }
