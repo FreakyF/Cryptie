070c8dda0337f04330f5b3b3865d0fca3e3e6a38
Jakub Babiarski <babiarski.jakub@gmail.com>
2025-07-02 16:12:28 +0200

    .

---
+++ src/Cryptie.Server.Tests/Features/ServerStatus/Services/ServerStatusServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Cryptie.Server.Features.ServerStatus.Services;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Cryptie.Server.Tests.Features.ServerStatus.Services;
+
+public class ServerStatusServiceTests
+{
+    [Fact]
+    public void GetServerStatus_ReturnsOkResult()
+    {
+        // Arrange
+        var service = new ServerStatusService();
+
+        // Act
+        var result = service.GetServerStatus();
+
+        // Assert
+        Assert.IsType<OkResult>(result);
+    }
+}
+
