d2ba7323912dd36219fc303a627ea6d7c6d25867
Jakub Babiarski <babiarski.jakub@gmail.com>
2025-07-02 16:14:44 +0200

    .

---
+++ src/Cryptie.Server.Tests/Features/ServerStatus/Controllers/ServerStatusControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Cryptie.Server.Features.ServerStatus.Controllers;
+using Cryptie.Server.Features.ServerStatus.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Cryptie.Server.Tests.Features.ServerStatus.Controllers;
+
+public class ServerStatusControllerTests
+{
+    [Fact]
+    public void GetServerStatus_CallsServiceAndReturnsResult()
+    {
+        // Arrange
+        var serviceMock = new Mock<IServerStatusService>();
+        var expectedResult = new OkResult();
+        serviceMock.Setup(s => s.GetServerStatus()).Returns(expectedResult);
+        var controller = new ServerStatusController(serviceMock.Object);
+
+        // Act
+        var result = controller.GetServerStatus();
+
+        // Assert
+        Assert.Same(expectedResult, result);
+        serviceMock.Verify(s => s.GetServerStatus(), Times.Once);
+    }
+}
+
