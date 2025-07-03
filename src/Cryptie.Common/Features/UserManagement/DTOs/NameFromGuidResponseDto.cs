132b19e6667553f88e8a316217df212f732c69a9
Jan Chojnacki <jan@chojnacki.dev>
2025-07-03 17:03:14 +0200

    fix Non-nullable property 'Name' must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring the property as nullable.

--- src/Cryptie.Common/Features/UserManagement/DTOs/NameFromGuidResponseDto.cs
+++ src/Cryptie.Common/Features/UserManagement/DTOs/NameFromGuidResponseDto.cs
@@ -2,5 +2,5 @@ namespace Cryptie.Common.Features.UserManagement.DTOs;
 
 public class NameFromGuidResponseDto
 {
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 }
