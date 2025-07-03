9d0ffe1588e68874f69c9d789d6d8c98746a0344
Jan Chojnacki <jan@chojnacki.dev>
2025-07-03 16:57:44 +0200

    fix Non-nullable property 'Messages' must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring the property as nullable.

--- src/Cryptie.Common/Features/Messages/DTOs/GetGroupMessagesSinceResponseDto.cs
+++ src/Cryptie.Common/Features/Messages/DTOs/GetGroupMessagesSinceResponseDto.cs
@@ -5,16 +5,15 @@ namespace Cryptie.Common.Features.Messages.DTOs
 {
     public class GetGroupMessagesSinceResponseDto
     {
-        public List<MessageDto> Messages { get; set; }
+        public List<MessageDto> Messages { get; set; } = [];
 
         public class MessageDto
         {
             public Guid MessageId { get; set; }
             public Guid GroupId { get; set; }
             public Guid SenderId { get; set; }
-            public string Message { get; set; }
+            public string Message { get; set; } = string.Empty;
             public DateTime DateTime { get; set; }
         }
     }
 }
-
