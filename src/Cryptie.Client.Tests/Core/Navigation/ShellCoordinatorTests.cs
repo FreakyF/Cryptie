13b58b1057a2f71d83b84d34825c1cdc58cc374e
Jan Chojnacki <jan@chojnacki.dev>
2025-07-03 17:46:34 +0200

    fix test

--- src/Cryptie.Client.Tests/Core/Navigation/ShellCoordinatorTests.cs
+++ src/Cryptie.Client.Tests/Core/Navigation/ShellCoordinatorTests.cs
@@ -1,113 +1,216 @@
-﻿using System.Net;
-using Cryptie.Client.Core.Dependencies;
+using System.Net;
+using System.Reflection;
+using System.Reactive.Linq;
 using Cryptie.Client.Core.Factories;
 using Cryptie.Client.Core.Navigation;
+using Cryptie.Client.Core.Dependencies;
 using Cryptie.Client.Core.Services;
 using Cryptie.Client.Features.Authentication.Services;
 using Cryptie.Client.Features.Authentication.State;
+using Cryptie.Common.Features.UserManagement.DTOs;
+using Cryptie.Client.Features.Authentication.ViewModels;
+using Cryptie.Client.Features.Dashboard.ViewModels;
 using Cryptie.Client.Features.Groups.State;
 using Cryptie.Client.Features.Menu.State;
-using Cryptie.Common.Features.UserManagement.DTOs;
-using Moq;
+using NSubstitute;
+using ReactiveUI;
 
-namespace Cryptie.Client.Tests.Core.Navigation;
-
-public class ShellCoordinatorTests
+namespace Cryptie.Client.Tests.Core.Navigation
 {
-    private readonly Mock<IViewModelFactory> _factory = new();
-    private readonly Mock<IKeychainManagerService> _keychain = new();
-    private readonly Mock<IUserDetailsService> _userDetails = new();
-    private readonly Mock<IConnectionMonitor> _connection = new();
-    private readonly ShellStateDependencies _stateDeps;
-    private readonly ShellCoordinator _coordinator;
-
-    private readonly Mock<IUserState> _userState = new();
-    private readonly Mock<IGroupSelectionState> _groupSelectionState = new();
-    private readonly Mock<ILoginState> _loginState = new();
-    private readonly Mock<IRegistrationState> _registrationState = new();
-
-    public ShellCoordinatorTests()
+    public class ShellCoordinatorTests
     {
-        _stateDeps = new ShellStateDependencies(
-            _userState.Object,
-            _groupSelectionState.Object,
-            _loginState.Object,
-            _registrationState.Object
-        );
-        _coordinator = new ShellCoordinator(_factory.Object, _keychain.Object, _userDetails.Object, _connection.Object, _stateDeps);
-    }
+        private readonly IViewModelFactory _factory = Substitute.For<IViewModelFactory>();
+        private readonly IKeychainManagerService _keychain = Substitute.For<IKeychainManagerService>();
+        private readonly IUserDetailsService _userDetails = Substitute.For<IUserDetailsService>();
+        private readonly IConnectionMonitor _connectionMonitor = Substitute.For<IConnectionMonitor>();
 
-    
+        private readonly ShellStateDependencies _stateDeps =
+            new ShellStateDependencies(
+                new UserState(),
+                new GroupSelectionState(),
+                new LoginState(),
+                new RegistrationState()
+            );
 
-    [Fact]
-    public void TryInitializeSession_Fails_ReturnsFalse()
-    {
-        var result = InvokeTryInitializeSession(out var token);
-        Assert.False(result);
-        Assert.Equal(Guid.Empty, token);
-    }
-
-    
-    [Fact]
-    public async Task GetUserGuidAsync_ReturnsGuid()
-    {
-        var guid = Guid.NewGuid();
-        _userDetails.Setup(x => x.GetUserGuidFromTokenAsync(It.IsAny<UserGuidFromTokenRequestDto>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(new UserGuidFromTokenResponseDto { Guid = guid });
-        var result = await InvokeGetUserGuidAsync(guid);
-        Assert.Equal(guid, result);
-    }
-
-    [Fact]
-    public async Task GetUserGuidAsync_ReturnsEmptyOnNull()
-    {
-        _userDetails.Setup(x => x.GetUserGuidFromTokenAsync(It.IsAny<UserGuidFromTokenRequestDto>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync((UserGuidFromTokenResponseDto)null!);
-        var result = await InvokeGetUserGuidAsync(Guid.NewGuid());
-        Assert.Equal(Guid.Empty, result);
-    }
-
-    [Fact]
-    public void TryInitializeUser_Fails_ReturnsFalse()
-    {
-        _keychain.Setup(x => x.TryGetPrivateKey(out It.Ref<string>.IsAny, out It.Ref<string>.IsAny)).Returns(false);
-        var result = InvokeTryInitializeUser(Guid.NewGuid());
-        Assert.False(result);
-    }
-
-    [Theory]
-    [InlineData(HttpStatusCode.Unauthorized, true)]
-    [InlineData(HttpStatusCode.Forbidden, true)]
-    [InlineData(HttpStatusCode.BadRequest, true)]
-    [InlineData(HttpStatusCode.NotFound, false)]
-    public void IsAuthError_Works(HttpStatusCode code, bool expected)
-    {
-        var ex = new HttpRequestException("fail", null, code);
-        var result = InvokeIsAuthError(ex);
-        Assert.Equal(expected, result);
-    }
-
-    // helpers
-    private Task<Guid> InvokeGetUserGuidAsync(Guid token) =>
-        (Task<Guid>)typeof(ShellCoordinator).GetMethod("GetUserGuidAsync", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)!.Invoke(_coordinator, new object[] { token });
-
-    private bool InvokeTryInitializeUser(Guid guid) =>
-        (bool)typeof(ShellCoordinator).GetMethod("TryInitializeUser", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)!.Invoke(_coordinator, new object[] { guid });
-
-    private void InvokeClearUserState() =>
-        typeof(ShellCoordinator).GetMethod("ClearUserState", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)!.Invoke(_coordinator, null);
-
-    private bool InvokeIsAuthError(HttpRequestException ex) =>
-        (bool)typeof(ShellCoordinator).GetMethod("IsAuthError", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)!.Invoke(null, new object[] { ex });
-
-    private bool InvokeTryInitializeSession(out Guid token)
-    {
-        token = Guid.Empty;
-        if (_keychain.Object.TryGetSessionToken(out var tokenStr, out var error) && Guid.TryParse(tokenStr, out var guid))
+        private ShellCoordinator CreateCoordinator()
         {
-            token = guid;
-            return true;
+            var coord = new ShellCoordinator(
+                _factory,
+                _keychain,
+                _userDetails,
+                _connectionMonitor,
+                _stateDeps
+            );
+
+            // --- STUB OUT NAVIGATION SO IT NEVER BOUNCES ON IRoutableViewModel CHECK ---
+            var stubNav =
+                ReactiveCommand.CreateFromObservable<IRoutableViewModel, IRoutableViewModel>(vm =>
+                    Observable.Return(vm));
+            var stubNavReset =
+                ReactiveCommand.CreateFromObservable<IRoutableViewModel, IRoutableViewModel>(vm =>
+                    Observable.Return(vm));
+
+            var router = coord.Router;
+            typeof(RoutingState)
+                .GetField("<Navigate>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic)!
+                .SetValue(router, stubNav);
+            typeof(RoutingState)
+                .GetField("<NavigateAndReset>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic)!
+                .SetValue(router, stubNavReset);
+
+            return coord;
         }
-        return false;
+
+        [Fact]
+        public async Task StartAsync_NoSessionToken_ResetsAndShowsLogin()
+        {
+            _keychain.TryGetSessionToken(out _, out _)
+                .ReturnsForAnyArgs(call =>
+                {
+                    call[0] = null;
+                    call[1] = null;
+                    return false;
+                });
+
+            var coord = CreateCoordinator();
+            await coord.StartAsync();
+
+            _factory.Received(1).Create<LoginViewModel>(coord);
+        }
+
+        [Fact]
+        public async Task StartAsync_BackendDown_ResetsAndShowsLogin()
+        {
+            var token = Guid.NewGuid().ToString();
+            _keychain.TryGetSessionToken(out _, out _)
+                .ReturnsForAnyArgs(call =>
+                {
+                    call[0] = token;
+                    call[1] = null;
+                    return true;
+                });
+            _connectionMonitor.IsBackendAliveAsync().Returns(false);
+
+            var coord = CreateCoordinator();
+            await coord.StartAsync();
+
+            _factory.Received(1).Create<LoginViewModel>(coord);
+        }
+
+        [Fact]
+        public async Task StartAsync_InvalidUserGuid_ResetsAndShowsLogin()
+        {
+            var token = Guid.NewGuid().ToString();
+            _keychain.TryGetSessionToken(out _, out _)
+                .ReturnsForAnyArgs(call =>
+                {
+                    call[0] = token;
+                    call[1] = null;
+                    return true;
+                });
+            _connectionMonitor.IsBackendAliveAsync().Returns(true);
+            _userDetails
+                .GetUserGuidFromTokenAsync(Arg.Any<UserGuidFromTokenRequestDto>())
+                .Returns(Task.FromResult(new UserGuidFromTokenResponseDto { Guid = Guid.Empty }));
+
+            var coord = CreateCoordinator();
+            await coord.StartAsync();
+
+            _factory.Received(1).Create<LoginViewModel>(coord);
+        }
+
+        [Fact]
+        public async Task StartAsync_MissingPrivateKey_ResetsAndShowsLogin()
+        {
+            var token = Guid.NewGuid().ToString();
+            _keychain.TryGetSessionToken(out _, out _)
+                .ReturnsForAnyArgs(call =>
+                {
+                    call[0] = token;
+                    call[1] = null;
+                    return true;
+                });
+            _connectionMonitor.IsBackendAliveAsync().Returns(true);
+            _userDetails
+                .GetUserGuidFromTokenAsync(Arg.Any<UserGuidFromTokenRequestDto>())
+                .Returns(Task.FromResult(new UserGuidFromTokenResponseDto { Guid = Guid.NewGuid() }));
+            _keychain.TryGetPrivateKey(out _, out _)
+                .ReturnsForAnyArgs(call =>
+                {
+                    call[0] = null;
+                    call[1] = null;
+                    return false;
+                });
+
+            var coord = CreateCoordinator();
+            await coord.StartAsync();
+
+            _factory.Received(1).Create<LoginViewModel>(coord);
+        }
+
+        [Fact]
+        public async Task StartAsync_AuthError_ResetsAndShowsLogin()
+        {
+            var token = Guid.NewGuid().ToString();
+            _keychain.TryGetSessionToken(out _, out _)
+                .ReturnsForAnyArgs(call =>
+                {
+                    call[0] = token;
+                    call[1] = null;
+                    return true;
+                });
+            _connectionMonitor.IsBackendAliveAsync().Returns(true);
+            _userDetails
+                .When(s => s.GetUserGuidFromTokenAsync(Arg.Any<UserGuidFromTokenRequestDto>()))
+                .Do(_ => throw new HttpRequestException("denied", null, HttpStatusCode.Unauthorized));
+
+            var coord = CreateCoordinator();
+            await coord.StartAsync();
+
+            _factory.Received(1).Create<LoginViewModel>(coord);
+        }
+
+        [Fact]
+        public async Task StartAsync_AllGood_ShowsDashboard()
+        {
+            var token = Guid.NewGuid().ToString();
+            var userGuid = Guid.NewGuid();
+
+            _keychain.TryGetSessionToken(out _, out _)
+                .ReturnsForAnyArgs(call =>
+                {
+                    call[0] = token;
+                    call[1] = null;
+                    return true;
+                });
+            _connectionMonitor.IsBackendAliveAsync().Returns(true);
+            _userDetails
+                .GetUserGuidFromTokenAsync(Arg.Any<UserGuidFromTokenRequestDto>())
+                .Returns(Task.FromResult(new UserGuidFromTokenResponseDto { Guid = userGuid }));
+            _keychain.TryGetPrivateKey(out _, out _)
+                .ReturnsForAnyArgs(call =>
+                {
+                    call[0] = "dummy";
+                    call[1] = null;
+                    return true;
+                });
+
+            var coord = CreateCoordinator();
+            await coord.StartAsync();
+
+            _factory.Received(1).Create<DashboardViewModel>(coord);
+        }
+
+        [Fact]
+        public void ShowMethods_DoNotThrow()
+        {
+            var coord = CreateCoordinator();
+            coord.ShowLogin();
+            coord.ShowRegister();
+            coord.ShowQrSetup();
+            coord.ShowTotpCode();
+            coord.ShowDashboard();
+            coord.ShowPinSetup();
+        }
     }
 }
