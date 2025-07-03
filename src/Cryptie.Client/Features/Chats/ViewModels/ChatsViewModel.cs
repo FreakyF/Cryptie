65aee5873ac8d1a4d5af0813294fd5e3a66b07f0
Kamil Fudala <kamil.fudala44@gmail.com>
2025-07-03 16:25:47 +0200

    Fix.

--- src/Cryptie.Client/Features/Chats/ViewModels/ChatsViewModel.cs
+++ src/Cryptie.Client/Features/Chats/ViewModels/ChatsViewModel.cs
@@ -197,7 +197,7 @@ public sealed class ChatsViewModel : RoutableViewModelBase, IActivatableViewMode
             }
             catch
             {
-                // Swallow exception: do nothing
+                // Swallow exception: do nothing 
             }
         }, canSend);
     }
