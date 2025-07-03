a45568967bfd66dcc48d1f9afacf7f44ea051808
Kamil Fudala <kamil.fudala44@gmail.com>
2025-07-03 19:26:30 +0200

    Add comment to silence Avalonia implicit usage.

--- src/Cryptie.Client/Features/Authentication/Views/LoginView.axaml.cs
+++ src/Cryptie.Client/Features/Authentication/Views/LoginView.axaml.cs
@@ -3,6 +3,7 @@ using Cryptie.Client.Features.Authentication.ViewModels;
 
 namespace Cryptie.Client.Features.Authentication.Views;
 
+// ReSharper disable once UnusedType.Global
 public partial class LoginView : ReactiveUserControl<LoginViewModel>
 {
     public LoginView()
